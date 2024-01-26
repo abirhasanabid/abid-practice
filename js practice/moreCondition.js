18c446282f2c505753bae87f2c04ddeb5bb08c3a
abirhasanabid <abid.7.tm@gmail.com>
2024-01-26 23:55:59 +0600

    new commit

---
+++ js practice/moreCondition.js
@@ -0,0 +1,17 @@
+const salary = 20000;
+const height = 65;
+const isBCS = true;
+const hasCar = true;
+// if(salary > 15000 && height > 70){
+//     console.log("Let's get married");
+// }
+// else{
+//     console.log("Let's not get married");
+// }
+
+if((salary > 20000 || height > 65 || isBCS == true) && (hasCar == true)){
+    console.log("bia korbo");
+}
+else{
+    console.log("bia korbo na ");
+}
