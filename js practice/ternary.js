23541b3e6d46693818a27317d5cc0a31dc19cf1e
abirhasanabid <abid.7.tm@gmail.com>
2024-01-27 12:37:05 +0600

    new commit

---
+++ js practice/ternary.js
@@ -0,0 +1,10 @@
+// const age = 12;
+// age >= 18 ? console.log('jaw vote dita') : console.log('muri khaw bose bose'
+
+
+let price = 11000;
+const isLeader = false;
+price = isLeader === true ?
+    price > 1000 ? price / 2 : 0 :
+    price + 1000;
+console.log(price);
