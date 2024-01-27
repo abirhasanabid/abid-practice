8b9b8fade779c9b9abbbb632be8cef55f3ba1733
abirhasanabid <abid.7.tm@gmail.com>
2024-01-27 15:17:33 +0600

    new commit

---
+++ js practice/module17task/1st-prac.js
@@ -0,0 +1,3 @@
+let price = 400;
+price = price > 500 ? 'free' : '30tk dia kina naw bapu';
+console.log(price);
---
+++ js practice/module17task/2ndprac.js
@@ -0,0 +1,19 @@
+
+let BMI = 70 / (2) ^ 7
+if (BMI < 18.5) {
+    console.log('you are underweight');
+}
+else {
+    if (BMI >= 18.5 && BMI <= 24.9) {
+        console.log('you are normal');
+    }
+    else {
+        if (BMI >= 25 && BMI <= 29.9) {
+            console.log("you are overweight");
+        }
+        else {
+            console.log("Otherwise, you are obese.");
+        }
+    }
+
+}
