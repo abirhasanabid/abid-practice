3047997e631abbd16886c2fbd0a2e7d3df8dde3e
abirhasanabid <abid.7.tm@gmail.com>
2024-01-27 16:07:50 +0600

    new commit

---
+++ js practice/module17task/5thPrac.js
@@ -0,0 +1,17 @@
+let num1 = 6;
+let num2 = 6;
+let result = num1 * num1;
+let sum = num1 + num2;
+// if(num1 > num2){
+//     console.log(result);
+// }
+// else{
+//     if(!(num1 > num2)){
+//         console.log(sum);
+//     }
+// }
+
+
+(num1 > num2) ? result : sum;
+console.log(result);
+
