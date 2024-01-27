12ac662c4b6d19d0589bdbf5ce9b52dc400720ce
abirhasanabid <abid.7.tm@gmail.com>
2024-01-27 16:10:58 +0600

    new commit

---
+++ js practice/logicalNotOprator.js
@@ -0,0 +1,45 @@
+// let isLeader = false;
+// if(!!isFinite){
+//     console.log('make a sit for him');
+// }
+// else{
+//     console.log('dhukta dish na');
+// }
+
+
+// let num1 = 4;
+// if(!!(num1)){
+//     console.log('onake vlo');
+// }
+// else{
+//     console.log('tmi onake kharap');
+// }
+
+// let x = 11;
+// if(x < 10){
+//     x = x + 3;
+// }
+// else{
+//     x = x - 2;
+// }
+// console.log(x);
+
+
+let grade = 60;
+// if(grade >= 60){
+//     console.log('pass');
+
+// }
+// else{
+//     console.log('fail');
+// }
+
+// grade = (grade >= 60) ? "pass" : "fail";
+// {
+//     console.log(grade);
+// }
+
+let result = 10 < "5" ;
+if (10 < "5"){
+    console.log(result);
+}
