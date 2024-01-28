e0c7a651ae5abb9fdd4c7328e4a118c74d6f6b8c
abirhasanabid <abid.7.tm@gmail.com>
2024-01-28 17:53:55 +0600

    new commit

--- js practice/module17task/5thPrac.js
+++ js practice/module17task/5thPrac.js
@@ -1,17 +1,16 @@
-let num1 = 6;
-let num2 = 6;
-let result = num1 * num1;
-let sum = num1 + num2;
-// if(num1 > num2){
-//     console.log(result);
-// }
-// else{
-//     if(!(num1 > num2)){
-//         console.log(sum);
-//     }
-// }
+let num1 = 4
+let num2 = 6
+let result
+
+if(num1 > num2){
+result = num1* num2
+    console.log(result);
+}
+else{
+result = num1 + num2
+        console.log(result);
+    }
 
 
-(num1 > num2) ? result : sum;
-console.log(result);
-
+result = (num1 > num2) ? num1* num2 : num1 + num2;
+console.log(result)
